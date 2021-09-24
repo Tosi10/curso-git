2a6c511654c8697a6569ed5014788362a14ea867
Rodrigo Tosi <rodrigotosi10@gmail.com>
2021-09-24 09:16:28 -0300

    Projeto iniciado, mostrando ola mundo

---
+++ Projetogit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projetogit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
